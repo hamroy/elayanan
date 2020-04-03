a2387992528e934f607c73ce338714f39c21c262
ilhamroy <hamroy1228@gmail.com>
2020-04-03 15:20:37 +0700

    up control C_master

--- application/controllers/C_master.php
+++ application/controllers/C_master.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class login_proses extends CI_Controller {
+class C_master extends CI_Controller {
 	public function index()
 	{
 
